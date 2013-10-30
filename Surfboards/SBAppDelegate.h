147a4a62512b432dab1842fb42e087b332049073
Kosuke Hata <hata.kosuke@gmail.com>
2013-10-29 18:09:01 -0700

    Initial Commit

---
+++ Surfboards/SBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SBAppDelegate.h
+//  Surfboards
+//
+//  Created by Kosuke Hata on 10/29/13.
+//  Copyright (c) 2013 canopy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Surfboards/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Surfboards
+//
+//  Created by Kosuke Hata on 10/29/13.
+//  Copyright (c) 2013 canopy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SBAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SBAppDelegate class]));
+    }
+}
